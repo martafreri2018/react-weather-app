7d710a11c2bee49bca6e69fcb42ce6ab0e406a11
martafreri2018 <73704811+martafreri2018@users.noreply.github.com>
2021-02-08 23:45:44 +0100

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container"> 
       <h1>Weather App </h1>
       <footer>This project was created by Marta Freri and is open-sourced on <a href="https://github.com/martafreri2018/react-weather-app" target="_blank"> Github </a>
        </footer>
+    </div>
     </div>
   );
 }
