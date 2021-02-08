b93f80d6cc2ea10453797cb1fb096b4ef90fff78
martafreri2018 <73704811+martafreri2018@users.noreply.github.com>
2021-02-08 23:19:55 +0100

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App </h1>
+      <footer>This project was created by Marta Freri and is open-sourced on <a href="https://github.com/martafreri2018/react-weather-app" target="_blank"> Github </a>
+       </footer>
     </div>
   );
 }
